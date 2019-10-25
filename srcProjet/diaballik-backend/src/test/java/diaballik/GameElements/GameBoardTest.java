7eb7ee17a5d1046cb43dcbe2acac011176adfc49
Julio SANTILARIO-BERTHILIER <julio.santilario-berthilier@insa-rennes.fr>
2019-10-25 18:54:28 +0200

    Fin des test du GameBord + suppression des dossier sur le git .idea et target

--- srcProjet/diaballik-backend/src/test/java/diaballik/GameElements/GameBoardTest.java
+++ srcProjet/diaballik-backend/src/test/java/diaballik/GameElements/GameBoardTest.java
@@ -19,12 +19,11 @@ class GameBoardTest {
     @BeforeEach
     void init() {
         board = new GameBoard(p1, p2);
-        board.move(p1, new ActionCoord(new Coordinate(4, 0), new Coordinate(4, 1)));
     }
 
     @Test
     void getPawn() {
-        assertEquals(board.getHumanPlayer().getPawns().get(0),board.getPawn(board.getHumanPlayer().getPawns().get(0).getPosition()).get());
+        assertEquals(board.getHumanPlayer().getPawns().get(0), board.getPawn(board.getHumanPlayer().getPawns().get(0).getPosition()).get());
     }
 
     @Test
@@ -41,17 +40,34 @@ class GameBoardTest {
     }
 
     @Test
-    void movePawnVertical() {
+    void cannotMove() {
+        assertFalse(board.canMove(p1, new ActionCoord(new Coordinate(0, 0), new Coordinate(1, 1))));
+    }
+
+    @Test
+    void movePawn() {
         assertTrue(board.move(p1, new ActionCoord(new Coordinate(0, 0), new Coordinate(0, 1))));
         assertTrue(board.getPawn(new Coordinate(0, 1)).isPresent());
         assertTrue(board.getPawn(new Coordinate(0, 0)).isEmpty());
     }
 
     @Test
+    void movePawnFalse() {
+        assertFalse(board.move(p1, new ActionCoord(new Coordinate(0, 0), new Coordinate(1, 1))));
+    }
+
+    @Test
+    void movePawnOutOfBount() {
+        assertThrows(IndexOutOfBoundsException.class, () -> {
+            board.move(p1, new ActionCoord(new Coordinate(0, 0), new Coordinate(-1, 0)));
+        });
+    }
+
+    @Test
     void moveBallHorizontal() {
         assertTrue(board.getPawn(new Coordinate(3, 0)).get().isBallOwner());
         assertTrue(board.move(p1, new ActionCoord(new Coordinate(3, 0), new Coordinate(2, 0))));
-        assertEquals(board.getPawn(new Coordinate(2, 0)).get(),board.getHumanPlayer().getBall());
+        assertEquals(board.getPawn(new Coordinate(2, 0)).get(), board.getHumanPlayer().getBall());
         assertTrue(board.getPawn(new Coordinate(2, 0)).get().isBallOwner());
         assertFalse(board.getPawn(new Coordinate(3, 0)).get().isBallOwner());
     }
@@ -60,10 +76,28 @@ class GameBoardTest {
     void moveBallVertical() {
         assertTrue(board.getPawn(new Coordinate(3, 0)).get().isBallOwner());
         assertTrue(board.move(p1, new ActionCoord(new Coordinate(3, 0), new Coordinate(2, 0))));
-        assertEquals(board.getPawn(new Coordinate(2, 0)).get(),board.getHumanPlayer().getBall());
+        assertEquals(board.getPawn(new Coordinate(2, 0)).get(), board.getHumanPlayer().getBall());
         assertTrue(board.getPawn(new Coordinate(2, 0)).get().isBallOwner());
         assertFalse(board.getPawn(new Coordinate(3, 0)).get().isBallOwner());
     }
+
+    @Test
+    void moveBallDiagonal() {
+        assertTrue(board.getPawn(new Coordinate(3, 0)).get().isBallOwner());
+        assertTrue(board.move(p1, new ActionCoord(new Coordinate(1, 0), new Coordinate(1, 1))));
+        assertTrue(board.move(p1, new ActionCoord(new Coordinate(2, 0), new Coordinate(2, 1))));
+        assertTrue(board.move(p1, new ActionCoord(new Coordinate(3, 0), new Coordinate(2, 1))));
+        assertTrue(board.getPawn(new Coordinate(2, 1)).get().isBallOwner());
+        assertFalse(board.getPawn(new Coordinate(3, 0)).get().isBallOwner());
+    }
+
+    @Test
+    void moveBallFalse() {
+        assertTrue(board.getPawn(new Coordinate(3, 0)).get().isBallOwner());
+        assertFalse(board.canMoveBall(p1.getBall(),p2.getBall()));
+        assertFalse(board.move(p1, new ActionCoord(new Coordinate(3, 0), new Coordinate(2, 1))));
+    }
+
 
     @Test
     void undo() {
@@ -111,7 +145,48 @@ class GameBoardTest {
     }
 
     @Test
-    void checkIfWon() {
+    void checkIfWonP1() {
+        assertFalse(board.checkIfWon());
+        //Mouvement du pion du joueur 2 de (0,6) en (1,5).
+        board.move(p2, new ActionCoord(new Coordinate(0, 6), new Coordinate(0, 5)));
+        board.move(p2, new ActionCoord(new Coordinate(0, 5), new Coordinate(1, 5)));
+        //Avancement du pion du joueur 1 de (0,0) en (0,6)
+        board.move(p1, new ActionCoord(new Coordinate(0, 0), new Coordinate(0, 1)));
+        board.move(p1, new ActionCoord(new Coordinate(0, 1), new Coordinate(0, 2)));
+        board.move(p1, new ActionCoord(new Coordinate(0, 2), new Coordinate(0, 3)));
+        board.move(p1, new ActionCoord(new Coordinate(0, 3), new Coordinate(0, 4)));
+        board.move(p1, new ActionCoord(new Coordinate(0, 4), new Coordinate(0, 5)));
+        board.move(p1, new ActionCoord(new Coordinate(0, 5), new Coordinate(0, 6)));
+        //Avancement du point du joueur 1 de (1,0) en (0,0)
+        board.move(p1, new ActionCoord(new Coordinate(1, 0), new Coordinate(0, 0)));
+        //Avancement du point du joueur 1 de (2,0) en (2,1)
+        board.move(p1, new ActionCoord(new Coordinate(2, 0), new Coordinate(2, 1)));
+        //Déplacement de la balle de (3,0) en (0,0), puis de (0,0) en (0,6).
+        board.move(p1, new ActionCoord(new Coordinate(3, 0), new Coordinate(0, 0)));
+        board.move(p1, new ActionCoord(new Coordinate(0, 0), new Coordinate(0, 6)));
+        assertTrue(board.checkIfWon());
+    }
 
+    @Test
+    void checkIfWonP2() {
+        assertFalse(board.checkIfWon());
+        //Mouvement du pion du joueur 1 de (0,1) en (1,1).
+        board.move(p1, new ActionCoord(new Coordinate(0, 0), new Coordinate(0, 1)));
+        board.move(p1, new ActionCoord(new Coordinate(0, 1), new Coordinate(1, 1)));
+        //Avancement du pion du joueur 2 de (0,6) en (0,0)
+        board.move(p2, new ActionCoord(new Coordinate(0, 6), new Coordinate(0, 5)));
+        board.move(p2, new ActionCoord(new Coordinate(0, 5), new Coordinate(0, 4)));
+        board.move(p2, new ActionCoord(new Coordinate(0, 4), new Coordinate(0, 3)));
+        board.move(p2, new ActionCoord(new Coordinate(0, 3), new Coordinate(0, 2)));
+        board.move(p2, new ActionCoord(new Coordinate(0, 2), new Coordinate(0, 1)));
+        board.move(p2, new ActionCoord(new Coordinate(0, 1), new Coordinate(0, 0)));
+        //Avancement du point du joueur 2 de (1,6) en (0,6)
+        board.move(p2, new ActionCoord(new Coordinate(1, 6), new Coordinate(0, 6)));
+        //Avancement du point du joueur 2 de (2,6) en (2,5)
+        board.move(p2, new ActionCoord(new Coordinate(2, 6), new Coordinate(2, 5)));
+        //Déplacement de la balle de (3,6) en (0,6), puis de (0,6) en (0,0).
+        board.move(p2, new ActionCoord(new Coordinate(3, 6), new Coordinate(0, 6)));
+        board.move(p2, new ActionCoord(new Coordinate(0, 6), new Coordinate(0, 0)));
+        assertTrue(board.checkIfWon());
     }
 }
