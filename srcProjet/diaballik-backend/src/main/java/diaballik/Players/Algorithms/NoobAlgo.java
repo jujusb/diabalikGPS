46376fc36e3bf2edd38b0cb2891bdb89761f7a07
Julio SANTILARIO-BERTHILIER <julio.santilario-berthilier@insa-rennes.fr>
2019-11-05 18:59:17 +0100

    Ajout des débuts du marchaling sur le player

--- srcProjet/diaballik-backend/src/main/java/diaballik/Players/Algorithms/NoobAlgo.java
+++ srcProjet/diaballik-backend/src/main/java/diaballik/Players/Algorithms/NoobAlgo.java
@@ -7,8 +7,6 @@ import java.util.List;
 import java.util.Random;
 
 public class NoobAlgo extends Algo {
-
-
     /**
      * Constructor of the algorith
      *
--- srcProjet/diaballik-backend/src/test/java/diaballik/model/TestMarshalling.java
+++ srcProjet/diaballik-backend/src/test/java/diaballik/model/TestMarshalling.java
@@ -9,52 +9,64 @@ import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
+
+import diaballik.Players.AiPlayer;
+import diaballik.Players.Algorithms.EAiType;
+import diaballik.Players.HumanPlayer;
+import diaballik.Players.Player;
 import org.eclipse.persistence.jaxb.JAXBContextFactory;
 import org.eclipse.persistence.jaxb.JAXBContextProperties;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 public class TestMarshalling {
-	<T> T marshall(final T objectToMarshall) throws IOException, JAXBException {
-		final Map<String, Object> properties = new HashMap<>();
-		properties.put(JAXBContextProperties.MEDIA_TYPE, "application/json");
-		properties.put(JAXBContextProperties.JSON_INCLUDE_ROOT, Boolean.TRUE);
+    <T> T marshall(final T objectToMarshall) throws IOException, JAXBException {
+        final Map<String, Object> properties = new HashMap<>();
+        properties.put(JAXBContextProperties.MEDIA_TYPE, "application/json");
+        properties.put(JAXBContextProperties.JSON_INCLUDE_ROOT, Boolean.TRUE);
 
-		final JAXBContext ctx = JAXBContextFactory.createContext(new Class[]{objectToMarshall.getClass()}, properties);
-		final Marshaller marshaller = ctx.createMarshaller();
-		marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+        final JAXBContext ctx = JAXBContextFactory.createContext(new Class[]{objectToMarshall.getClass()}, properties);
+        final Marshaller marshaller = ctx.createMarshaller();
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
 
-		final StringWriter sw = new StringWriter();
-		marshaller.marshal(objectToMarshall, sw);
-		marshaller.marshal(objectToMarshall, System.out);
+        final StringWriter sw = new StringWriter();
+        marshaller.marshal(objectToMarshall, sw);
+        marshaller.marshal(objectToMarshall, System.out);
 
-		final Unmarshaller unmarshaller = ctx.createUnmarshaller();
-		final StringReader reader = new StringReader(sw.toString());
-		final T o = (T) unmarshaller.unmarshal(reader);
+        final Unmarshaller unmarshaller = ctx.createUnmarshaller();
+        final StringReader reader = new StringReader(sw.toString());
+        final T o = (T) unmarshaller.unmarshal(reader);
 
-		sw.close();
-		reader.close();
+        sw.close();
+        reader.close();
 
-		return o;
-	}
+        return o;
+    }
 
-	// Player p1;
+    Player p1;
+    Player p2;
 
-	@BeforeEach
-	void setUp() {
-		// p1 = new Player("foo", "red");
-	}
+    @BeforeEach
+    void setUp() {
+        p1 = new HumanPlayer("foo", true);
+        p2 = new AiPlayer(EAiType.NOOB,"foo", true);
+    }
 
-	@Test
-	void testPlayer() throws IOException, JAXBException {
-		// final Player p = marshall(p1);
-		// assertEquals("foo", p.getName());
-		// assertEquals("red", p.getColor());
-	}
+    @Test
+    void testHumanPlayer() throws IOException, JAXBException {
+        final Player p = marshall(p1);
+        assertEquals("foo", p.getName());
+        assertTrue(p.getColor());
+    }
+
+//    @Test
+//    void testAiPlayer() throws IOException, JAXBException {
+//        final Player p = marshall(p2);
+//        assertEquals("foo", p.getName());
+//        assertTrue(p.getColor());
+//    }
 }
