36d27ac81f9b1b8351196a34a2338ed048b7db9f
Julio Santilario Berthilier <julio.santilario-berthilier@insa-rennes.fr>
2019-11-26 17:34:22 +0100

    Avancement du frontend pour l'affichage de la grille

--- srcProjet/diaballik-frontend/src/app/diabalik/diabalik.component.ts
+++ srcProjet/diaballik-frontend/src/app/diabalik/diabalik.component.ts
@@ -6,6 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./diabalik.component.css']
 })
 export class DiabalikComponent implements OnInit {
+  /* TODO :
+- remplir d'attributs décrivant le jeu cette classe
+- créer une ou des classes pour aider au premier point (ex : la classe case comme dans la ligne commentée du dessous)
+- dans le onInit, afficher les pions soit en touchant au HTML soit en utilisant ce qui est indiqué diapo 28
+- créer un parser json pour remplir les attributs
+- créer un évènement pour la réception de JSON et y afficher le jeu
+  */
+ // board: List<Case>;
+
 
   constructor() { }
 
