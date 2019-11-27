21a601f24afdf7817ec8d32aa051d4567f416151
Julio Santilario Berthilier <julio.santilario-berthilier@insa-rennes.fr>
2019-11-27 23:41:26 +0100

    Ajout des pions et de la balle sur le board

--- srcProjet/diaballik-frontend/src/app/diabalik/diabalik.component.ts
+++ srcProjet/diaballik-frontend/src/app/diabalik/diabalik.component.ts
@@ -8,10 +8,6 @@ import { MyData } from '../mydata';
 })
 export class DiabalikComponent implements OnInit {
   /* TODO :
-- remplir d'attributs décrivant le jeu cette classe
-- créer une ou des classes pour aider au premier point (ex : la classe case comme dans la ligne commentée du dessous)
-- dans le onInit, afficher les pions soit en touchant au HTML soit en utilisant ce qui est indiqué diapo 28
-- créer un parser json pour remplir les attributs
 - créer un évènement pour la réception de JSON et y afficher le jeu
   */
  // board: List<Case>;
