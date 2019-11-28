f285ba50abcf5f8648d27a4f3b531979d53fb704
pierre-yves.le-rolland-raumer <pierre-yves.le-rolland-raumer@insa-rennes.fr>
2019-11-28 20:05:24 +0100

    ajout de fonctions ts pour qu'une seule checkbox de couleur puisse être checkée à la fois, ajout d'une fonction pour générer la requête REST lorsque l'on clique sur Play. Suppression des nombreux <br> du html pour gérer cela dans le css

--- srcProjet/diaballik-frontend/src/app/mydata.ts
+++ srcProjet/diaballik-frontend/src/app/mydata.ts
@@ -15,4 +15,8 @@ export class MyData {
         // The JSON class can both parse JSON from a string, a produce a string from a JSON object (stringify)
         this.storage = JSON.parse('{"nbActions":0,"currentTurn":0,"gameBoard":{"undoable_mode":[],"redoable_mode":[],"board":[{"position":{"posX":0,"posY":0},"ballOwner":false,"player":"white"},{"position":{"posX":1,"posY":0},"ballOwner":false,"player":"white"},{"position":{"posX":2,"posY":0},"ballOwner":false,"player":"white"},{"position":{"posX":3,"posY":0},"ballOwner":true,"player":"white"},{"position":{"posX":4,"posY":0},"ballOwner":false,"player":"white"},{"position":{"posX":5,"posY":0},"ballOwner":false,"player":"white"},{"position":{"posX":6,"posY":0},"ballOwner":false,"player":"white"},null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,{"position":{"posX":0,"posY":6},"ballOwner":false,"player":"black"},{"position":{"posX":1,"posY":6},"ballOwner":false,"player":"black"},{"position":{"posX":2,"posY":6},"ballOwner":false,"player":"black"},{"position":{"posX":3,"posY":6},"ballOwner":true,"player":"black"},{"position":{"posX":4,"posY":6},"ballOwner":false,"player":"black"},{"position":{"posX":5,"posY":6},"ballOwner":false,"player":"black"},{"position":{"posX":6,"posY":6},"ballOwner":false,"player":"black"}]},"player1":{"type":"humanPlayer","name":"Bob","colour":true,"winner":false},"player2":{"type":"humanPlayer","name":"Alice","colour":false,"winner":false},"currentPlayer":{"type":"humanPlayer","name":"Bob","colour":true,"winner":false}}');
     }
+
+    public receiveJson(json : string){
+        this.storage = JSON.parse(json);
+    }
 }
